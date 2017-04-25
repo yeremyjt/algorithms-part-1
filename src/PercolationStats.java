424c4794812ad53702bf72269c7bba9ff0da2930
Yeremy Turcios <yturcios@ancestry.com>
2017-04-24 18:50:09 -0600

    Removing comments.

--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -9,10 +9,6 @@ public class PercolationStats {
     private double meanValue;
     private double stddevValue;
 
-    /** perform T independent computational experiments on an N-by-N grid
-     * @param n grid of NxN size
-     * @param t number of times experiment is executed
-     */
     public PercolationStats(int n, int t) {
         if (n <= 0 || t <= 0)
             throw new java.lang.IllegalArgumentException();
@@ -46,38 +42,23 @@ public class PercolationStats {
         stddevValue = stddev();
     }
 
-    /** sample mean of percolation threshold
-     * @return sample mean
-     */
     public double mean() {
         return StdStats.mean(x);
 
     }
 
-    /** sample standard deviation of percolation threshold
-     * @return sample standard deviation
-     */
     public double stddev() {
         return StdStats.stddev(x);
     }
 
-    /** returns lower bound of the 95% confidence interval
-     * @return lower bound of the 95% confidence interval
-     */
     public double confidenceLo() {
         return meanValue - ((1.96 * stddevValue) / Math.sqrt(times));
     }
 
-    /** returns upper bound of the 95% confidence interva
-     * @return upper bound of the 95% confidence interval
-     */
     public double confidenceHi() {
         return meanValue + ((1.96 * stddevValue) / Math.sqrt(times));
     }
 
-    /** test client
-     * @param args
-     */
     public static void main(String[] args) {
 
         int n = 200;
@@ -86,7 +67,5 @@ public class PercolationStats {
         StdOut.println("mean = " + ps.mean());
         StdOut.println("stddev = " + ps.stddev());
         StdOut.println("95% confidence interval =  " + ps.confidenceLo() + ", " + ps.confidenceHi());
-
     }
-
 }
